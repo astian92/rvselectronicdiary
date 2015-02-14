1e692e86dbbf52fdfa75de4b56a1e20342cc7b9c
bork0o0131 <bork0o0131@gmail.com>
2015-02-14 16:21:10 +0000

    14/2/2015

--- RVS/RED/RED/App_Start/BundleConfig.cs
+++ RVS/RED/RED/App_Start/BundleConfig.cs
@@ -8,28 +8,48 @@ namespace RED
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //// CSS style (bootstrap/inspinia)
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.min.css",
+            //          "~/Content/animate.css",
+            //          "~/Content/style.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            //// Font Awesome icons
+            //bundles.Add(new StyleBundle("~/font-awesome/css").Include(
+            //          "~/fonts/font-awesome/css/font-awesome.min.css"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //// Inspinia script
+            //bundles.Add(new ScriptBundle("~/bundles/inspinia").Include(
+            //          "~/Scripts/plugins/metisMenu/metisMenu.min.js",
+            //          "~/Scripts/plugins/pace/pace.min.js",
+            //          "~/Scripts/app/inspinia.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            //// jQuery
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-2.1.1.min.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
 
-            // Set EnableOptimizations to false for debugging. For more information,
-            // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            BundleTable.EnableOptimizations = true;
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
+
+            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
+            //// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/respond.js"));
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
+
+            //// Set EnableOptimizations to false for debugging. For more information,
+            //// visit http://go.microsoft.com/fwlink/?LinkId=301862
+            //BundleTable.EnableOptimizations = true;
         }
     }
 }
