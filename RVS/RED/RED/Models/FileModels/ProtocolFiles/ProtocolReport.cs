f1c651ce39cb80f657aa937a998f6d23054732e3
astian <abrashev92@gmail.com>
2016-05-24 20:45:01 +0300

    before updating the tool

--- RVS/RED/RED/Models/FileModels/ProtocolFiles/ProtocolReport.cs
+++ RVS/RED/RED/Models/FileModels/ProtocolFiles/ProtocolReport.cs
@@ -15,6 +15,7 @@ namespace RED.Models.FileModels.ProtocolFiles
     public class ProtocolReport : DocXReportBase
     {
         private IOrderedEnumerable<ProtocolResult> modelItems;
+        private double[] cellsWidth = { 2d, 2d, 2d, 2d, 2d, 2d, 2d, 2d, 2d };
 
         private bool hasMKB;
         private bool hasFZH;
@@ -41,12 +42,14 @@ namespace RED.Models.FileModels.ProtocolFiles
             InsertLists();
 
             var secondDocument = CreateLandscapePart();
+            var protocolResults = ReportModel.ReportParameters["ProtocolResults"] as IOrderedEnumerable<ProtocolResult>;
 
             string tableTitle = string.Empty;
             if (this.hasMKB)
             {
                 tableTitle = "7.1 РЕЗУЛТАТИ ОТ МИКРОБИОЛОГИЧНО ИЗПИТВАНЕ:";
-                CreateResultsTable(secondDocument, tableTitle);
+                var data = protocolResults.Where(pr => pr.ProductTest.Test.TestType.ShortName == TestTypes.MKB);
+                CreateResultsTable(secondDocument, tableTitle, data);
             }
             if (this.hasFZH)
             {
@@ -57,7 +60,8 @@ namespace RED.Models.FileModels.ProtocolFiles
                 }
 
                 tableTitle = "7." + number + " РЕЗУЛТАТИ ОТ ФИЗИКОХИМИЧНО И ОРГАНОЛЕПТИЧНО ИЗПИТВАНЕ:";
-                CreateResultsTable(secondDocument, tableTitle);
+                var data = protocolResults.Where(pr => pr.ProductTest.Test.TestType.ShortName == TestTypes.FZH);
+                CreateResultsTable(secondDocument, tableTitle, data);
             }
             
             secondDocument.Save();
@@ -154,11 +158,12 @@ namespace RED.Models.FileModels.ProtocolFiles
             titleStyle.FontFamily = new System.Drawing.FontFamily("Times New Roman");
 
             Paragraph p = document2.InsertParagraph("7. РЕЗУЛТАТИ ОТ ИЗПИТВАНЕ", false, titleStyle);
+            p.IndentationBefore = 2;
 
             return document2;
         }
 
-        private void CreateResultsTable(DocX document2, string tableTitle) //add items later
+        private void CreateResultsTable(DocX document2, string tableTitle, IEnumerable<ProtocolResult> data) //add items later
         {
             var titleStyle = new Formatting();
             titleStyle.Size = 12;
@@ -166,26 +171,18 @@ namespace RED.Models.FileModels.ProtocolFiles
             titleStyle.FontFamily = new System.Drawing.FontFamily("Times New Roman");
 
             var title = document2.InsertParagraph(tableTitle, false, titleStyle);
-            var protocolResults = ReportModel.ReportParameters["ProtocolResults"] as IOrderedEnumerable<ProtocolResult>;
+            title.IndentationBefore = 2;
+            title.InsertText(Environment.NewLine);
 
             var table = document2.InsertTable(1, 9);
+            //table.AutoFit = AutoFit.Window;
 
             InsertTableHeader(table);
+            InsertTableNumerationRow(table);
 
-            //foreach (var item in this.modelItems)
-            //{
-            //    var row = table.InsertRow();
+            PopulateTable(table, data);
 
-            //    row.Cells[0].InsertParagraph("ASD");
-            //    row.Cells[1].InsertParagraph("ASD");
-            //    row.Cells[2].InsertParagraph("ASD");
-            //    row.Cells[3].InsertParagraph("ASD");
-            //    row.Cells[4].InsertParagraph("ASD");
-            //    row.Cells[5].InsertParagraph("ASD");
-            //    row.Cells[6].InsertParagraph("ASD");
-            //    row.Cells[7].InsertParagraph("ASD");
-            //    row.Cells[8].InsertParagraph("ASD");
-            //}
+            PrepareCells(table);
         }
 
         private void InsertTableHeader(Table table)
@@ -197,26 +194,70 @@ namespace RED.Models.FileModels.ProtocolFiles
             var row = table.Rows[0];
 
             row.Cells[0].Paragraphs[0].InsertText("№ по ред", false, textStyle);
-            
-            PrepareTableCell(row.Cells[0], 0.83);
             row.Cells[1].Paragraphs[0].InsertText("№ на образеца по вх/изх. дневник", false, textStyle);
-            PrepareTableCell(row.Cells[1], 1.97);
             row.Cells[2].Paragraphs[0].InsertText("Продукт", false, textStyle);
-            PrepareTableCell(row.Cells[2], 6);
             row.Cells[3].Paragraphs[0].InsertText("Изпитван показател", false, textStyle);
-            PrepareTableCell(row.Cells[3], 2.99);
             row.Cells[4].Paragraphs[0].InsertText("Единица на величина", false, textStyle);
-            PrepareTableCell(row.Cells[4], 1.74);
             row.Cells[5].Paragraphs[0].InsertText("Метод на изследване", false, textStyle);
-            PrepareTableCell(row.Cells[5], 0.83);
             row.Cells[6].Paragraphs[0].InsertText("Резултат от изпитването", false, textStyle);
-            PrepareTableCell(row.Cells[6], 0.83);
             row.Cells[7].Paragraphs[0].InsertText("Стойност и допуск на показателя", false, textStyle);
-            PrepareTableCell(row.Cells[7], 0.83);
             row.Cells[8].Paragraphs[0].InsertText("Условия на изпитването", false, textStyle);
-            PrepareTableCell(row.Cells[8], 0.83);
+        }
 
-            SetBorder(row);
+        private void InsertTableNumerationRow(Table table)
+        {
+            var textStyle = new Formatting();
+            textStyle.Size = 9;
+            textStyle.Bold = true;
+            textStyle.FontFamily = new System.Drawing.FontFamily("Times New Roman");
+
+            var row = table.InsertRow();
+
+            for (int i = 0; i < table.ColumnCount; i++)
+            {
+                row.Cells[i].Paragraphs[0].InsertText((i + 1).ToString(), false, textStyle);
+            }
+        }
+
+        private void PopulateTable(Table table, IEnumerable<ProtocolResult> data)
+        {
+            var textStyle = new Formatting();
+            textStyle.Size = 9;
+            textStyle.FontFamily = new System.Drawing.FontFamily("Times New Roman");
+
+            int productIndex = 1;
+            
+            foreach (var item in data)
+            {
+                var row = table.InsertRow();
+
+                row.Cells[0].Paragraphs[0].InsertText(productIndex + ".", false, textStyle);
+                row.Cells[1].Paragraphs[0].InsertText(item.ResultNumber, false, textStyle);
+                row.Cells[2].Paragraphs[0].InsertText(item.ProductTest.Product.Name, false, textStyle);
+                row.Cells[3].Paragraphs[0].InsertText(item.ProductTest.Test.Name, false, textStyle);
+                row.Cells[4].Paragraphs[0].InsertText(item.ProductTest.Test.UnitName, false, textStyle);
+                row.Cells[5].Paragraphs[0].InsertText(item.ProductTest.Test.TestMethods, false, textStyle);
+                row.Cells[6].Paragraphs[0].InsertText(item.Results, false, textStyle);
+                row.Cells[7].Paragraphs[0].InsertText(item.ProductTest.MethodValue, false, textStyle);
+                row.Cells[8].Paragraphs[0].InsertText(item.ProductTest.Test.Temperature, false, textStyle);
+                //rowIndex++;
+            }
+        }
+
+        private void PrepareCells(Table table)
+        {
+            foreach (var row in table.Rows)
+            {
+                for (int i = 0; i < table.ColumnCount; i++)
+                {
+                    var cell = row.Cells[i];
+                    cell.Width = cellsWidth[i];
+                    cell.Paragraphs[0].Alignment = Alignment.center;
+                    cell.VerticalAlignment = VerticalAlignment.Center;
+                }
+
+                SetBorder(row);
+            }
         }
 
         private void SetBorder(Row row)
@@ -235,12 +276,6 @@ namespace RED.Models.FileModels.ProtocolFiles
             cell.SetBorder(TableCellBorderType.Right, new Border(BorderStyle.Tcbs_single, BorderSize.one, 0, Color.Black));
         }
 
-        private void PrepareTableCell(Cell cell, double width)
-        {
-            cell.Width = width;
-            cell.Paragraphs[0].Alignment = Alignment.center;
-            cell.VerticalAlignment = VerticalAlignment.Center;
-        }
 
 
 
@@ -249,71 +284,73 @@ namespace RED.Models.FileModels.ProtocolFiles
 
 
 
-        private void InsertTable()
-        {
-            var cellStyle0 = new Formatting();
-            cellStyle0.Size = 12;
 
-            var cellStyle1 = new Formatting();
-            cellStyle1.Size = 12;
-            cellStyle1.Italic = true;
 
-            var cellStyle3 = new Formatting();
-            cellStyle3.Size = 10;
+        //private void InsertTable()
+        //{
+        //    var cellStyle0 = new Formatting();
+        //    cellStyle0.Size = 12;
 
-            var protocolResults = ReportModel.ReportParameters["ProtocolResults"] as IOrderedEnumerable<ProtocolResult>;
-            var table = Document.Tables.First();
-            int rowIndex = 2;
-            foreach (var result in protocolResults)
-            {
-                if (rowIndex > 2)
-                {
-                    var row = table.InsertRow();
-                    row.Cells[0].Width = 0.83;
-                    row.Cells[0].VerticalAlignment = VerticalAlignment.Center;
-                    row.Cells[1].Width = 2.99;
-                    row.Cells[1].VerticalAlignment = VerticalAlignment.Center;
-                    row.Cells[2].Width = 1.74;
-                    row.Cells[2].VerticalAlignment = VerticalAlignment.Center;
-                    row.Cells[3].Width = 4;
-                    row.Cells[3].VerticalAlignment = VerticalAlignment.Center;
-                    row.Cells[4].Width = 1.97;
-                    row.Cells[4].VerticalAlignment = VerticalAlignment.Center;
-                    row.Cells[5].Width = 4.75;
-                    row.Cells[5].VerticalAlignment = VerticalAlignment.Center;
-                    row.Cells[6].Width = 2.25;
-                    row.Cells[6].VerticalAlignment = VerticalAlignment.Center;
-                    row.Cells[7].Width = 2;
-                    row.Cells[7].VerticalAlignment = VerticalAlignment.Center;
-                }
+        //    var cellStyle1 = new Formatting();
+        //    cellStyle1.Size = 12;
+        //    cellStyle1.Italic = true;
 
-                table.Rows[rowIndex].Cells[0].Paragraphs[0].InsertText((rowIndex - 1) + ".", false, cellStyle0);
-                table.Rows[rowIndex].Cells[0].Paragraphs[0].Alignment = Alignment.center;
+        //    var cellStyle3 = new Formatting();
+        //    cellStyle3.Size = 10;
 
-                table.Rows[rowIndex].Cells[1].Paragraphs[0].InsertText(result.ProductTest.Test.Name, false, cellStyle1);
-                table.Rows[rowIndex].Cells[1].Paragraphs[0].Alignment = Alignment.center;
+        //    var protocolResults = ReportModel.ReportParameters["ProtocolResults"] as IOrderedEnumerable<ProtocolResult>;
+        //    var table = Document.Tables.First();
+        //    int rowIndex = 2;
+        //    foreach (var result in protocolResults)
+        //    {
+        //        if (rowIndex > 2)
+        //        {
+        //            var row = table.InsertRow();
+        //            row.Cells[0].Width = 0.83;
+        //            row.Cells[0].VerticalAlignment = VerticalAlignment.Center;
+        //            row.Cells[1].Width = 2.99;
+        //            row.Cells[1].VerticalAlignment = VerticalAlignment.Center;
+        //            row.Cells[2].Width = 1.74;
+        //            row.Cells[2].VerticalAlignment = VerticalAlignment.Center;
+        //            row.Cells[3].Width = 4;
+        //            row.Cells[3].VerticalAlignment = VerticalAlignment.Center;
+        //            row.Cells[4].Width = 1.97;
+        //            row.Cells[4].VerticalAlignment = VerticalAlignment.Center;
+        //            row.Cells[5].Width = 4.75;
+        //            row.Cells[5].VerticalAlignment = VerticalAlignment.Center;
+        //            row.Cells[6].Width = 2.25;
+        //            row.Cells[6].VerticalAlignment = VerticalAlignment.Center;
+        //            row.Cells[7].Width = 2;
+        //            row.Cells[7].VerticalAlignment = VerticalAlignment.Center;
+        //        }
 
-                table.Rows[rowIndex].Cells[2].Paragraphs[0].InsertText(result.ProductTest.Test.UnitName, false, cellStyle0);
-                table.Rows[rowIndex].Cells[2].Paragraphs[0].Alignment = Alignment.center;
+        //        table.Rows[rowIndex].Cells[0].Paragraphs[0].InsertText((rowIndex - 1) + ".", false, cellStyle0);
+        //        table.Rows[rowIndex].Cells[0].Paragraphs[0].Alignment = Alignment.center;
 
-                table.Rows[rowIndex].Cells[3].Paragraphs[0].InsertText(result.ProductTest.Test.TestMethods, false, cellStyle0);
-                table.Rows[rowIndex].Cells[3].Paragraphs[0].Alignment = Alignment.center;
+        //        table.Rows[rowIndex].Cells[1].Paragraphs[0].InsertText(result.ProductTest.Test.Name, false, cellStyle1);
+        //        table.Rows[rowIndex].Cells[1].Paragraphs[0].Alignment = Alignment.center;
 
-                table.Rows[rowIndex].Cells[4].Paragraphs[0].InsertText(result.ResultNumber, false, cellStyle0);
-                table.Rows[rowIndex].Cells[4].Paragraphs[0].Alignment = Alignment.center;
+        //        table.Rows[rowIndex].Cells[2].Paragraphs[0].InsertText(result.ProductTest.Test.UnitName, false, cellStyle0);
+        //        table.Rows[rowIndex].Cells[2].Paragraphs[0].Alignment = Alignment.center;
 
-                table.Rows[rowIndex].Cells[5].Paragraphs[0].InsertText(result.Results, false, cellStyle0);
-                table.Rows[rowIndex].Cells[5].Paragraphs[0].Alignment = Alignment.center;
+        //        table.Rows[rowIndex].Cells[3].Paragraphs[0].InsertText(result.ProductTest.Test.TestMethods, false, cellStyle0);
+        //        table.Rows[rowIndex].Cells[3].Paragraphs[0].Alignment = Alignment.center;
 
-                //table.Rows[rowIndex].Cells[6].Paragraphs[0].InsertText(result.MethodValue, false, cellStyle0);
-                //table.Rows[rowIndex].Cells[6].Paragraphs[0].Alignment = Alignment.center;
+        //        table.Rows[rowIndex].Cells[4].Paragraphs[0].InsertText(result.ResultNumber, false, cellStyle0);
+        //        table.Rows[rowIndex].Cells[4].Paragraphs[0].Alignment = Alignment.center;
 
-                table.Rows[rowIndex].Cells[7].Paragraphs[0].InsertText(result.ProductTest.Test.Temperature, false, cellStyle3);
-                table.Rows[rowIndex].Cells[7].Paragraphs[0].Alignment = Alignment.center;
+        //        table.Rows[rowIndex].Cells[5].Paragraphs[0].InsertText(result.Results, false, cellStyle0);
+        //        table.Rows[rowIndex].Cells[5].Paragraphs[0].Alignment = Alignment.center;
 
-                rowIndex++;
-            }
-        }
+        //        //table.Rows[rowIndex].Cells[6].Paragraphs[0].InsertText(result.MethodValue, false, cellStyle0);
+        //        //table.Rows[rowIndex].Cells[6].Paragraphs[0].Alignment = Alignment.center;
+
+        //        table.Rows[rowIndex].Cells[7].Paragraphs[0].InsertText(result.ProductTest.Test.Temperature, false, cellStyle3);
+        //        table.Rows[rowIndex].Cells[7].Paragraphs[0].Alignment = Alignment.center;
+
+        //        rowIndex++;
+        //    }
+        //}
 
         private void InsertRemarks()
         {
