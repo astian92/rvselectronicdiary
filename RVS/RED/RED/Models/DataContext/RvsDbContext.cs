30d581616b2ea0233c857d8cc66c70d59541de55
bork0o <bork0o0131@gmail.com>
2017-07-26 11:23:30 +0300

    removed custom logging on save changes

--- RVS/RED/RED/Models/DataContext/RvsDbContext.cs
+++ RVS/RED/RED/Models/DataContext/RvsDbContext.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using RED.Models.Account;
-using RED.Models.DataContext.Logging;
+﻿using System.Data.Entity;
 using RED.Models.ElectronicDiary.Clients;
 using RED.Models.ElectronicDiary.Requests;
 using RED.Models.ElectronicDiary.Tests;
@@ -20,55 +15,55 @@ namespace RED.Models.DataContext
 
         public DbSet<RequestW> RequestWs { get; set; }
 
-        public override int SaveChanges()
-        {
-            IEnumerable<ActionLog> logs = new List<ActionLog>();
-            bool preLoggingWentOk = true;
+        //public override int SaveChanges()
+        //{
+        //    IEnumerable<ActionLog> logs = new List<ActionLog>();
+        //    bool preLoggingWentOk = true;
 
-            //Prepare loggs (before commiting because states and oldValues change after commit!
-            try
-            {
-                string username = RvsPrincipal.User.Username;
-                var user = this.Users.FirstOrDefault(u => u.Username == username);
+        //    //Prepare loggs (before commiting because states and oldValues change after commit!
+        //    try
+        //    {
+        //        string username = RvsPrincipal.User.Username;
+        //        var user = this.Users.FirstOrDefault(u => u.Username == username);
 
-                if (user == null)
-                {
-                    throw new NotSupportedException("The action could not be performed for a not authenticated user!");
-                }
+        //        if (user == null)
+        //        {
+        //            throw new NotSupportedException("The action could not be performed for a not authenticated user!");
+        //        }
 
-                this.ChangeTracker.DetectChanges();
-                var changes = this.ChangeTracker.Entries().ToList();
+        //        this.ChangeTracker.DetectChanges();
+        //        var changes = this.ChangeTracker.Entries().ToList();
 
-                var logger = new Logger(user.Id);
-                logger.SetChanges(changes);
-                logs = logger.GetLogs();
-            }
-            catch (Exception exc)
-            {
-                var exception = new Exception("There was a problem with logging preparation. See inner exception for more details", exc);
-                Elmah.ErrorSignal.FromCurrentContext().Raise(exception);
-                preLoggingWentOk = false;
-            }
+        //        var logger = new Logger(user.Id);
+        //        logger.SetChanges(changes);
+        //        logs = logger.GetLogs();
+        //    }
+        //    catch (Exception exc)
+        //    {
+        //        var exception = new Exception("There was a problem with logging preparation. See inner exception for more details", exc);
+        //        Elmah.ErrorSignal.FromCurrentContext().Raise(exception);
+        //        preLoggingWentOk = false;
+        //    }
 
-            //save actual changes
-            int changesCount = base.SaveChanges();
+        //    //save actual changes
+        //    int changesCount = base.SaveChanges();
 
-            //use the same context to commit the logs
-            try
-            {
-                if (preLoggingWentOk)
-                {
-                    this.ActionLogs.AddRange(logs);
-                    base.SaveChanges(); //Again this time for the logs
-                }
-            }
-            catch (Exception exc)
-            {
-                var exception = new Exception("There was a problem with the logging. See inner exception for more details", exc);
-                Elmah.ErrorSignal.FromCurrentContext().Raise(exception);
-            }
+        //    //use the same context to commit the logs
+        //    try
+        //    {
+        //        if (preLoggingWentOk)
+        //        {
+        //            this.ActionLogs.AddRange(logs);
+        //            base.SaveChanges(); //Again this time for the logs
+        //        }
+        //    }
+        //    catch (Exception exc)
+        //    {
+        //        var exception = new Exception("There was a problem with the logging. See inner exception for more details", exc);
+        //        Elmah.ErrorSignal.FromCurrentContext().Raise(exception);
+        //    }
 
-            return changesCount;
-        }
+        //    return changesCount;
+        //}
     }
 }
