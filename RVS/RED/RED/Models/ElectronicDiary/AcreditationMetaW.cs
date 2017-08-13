4b4b5a6113f1b0281610c30f9f0a85923f3b6606
Martin Abrashev <Martin Abrashev>
2017-08-13 12:08:32 +0300

    wtf

--- RVS/RED/RED/Models/ElectronicDiary/AcreditationMetaW.cs
+++ RVS/RED/RED/Models/ElectronicDiary/AcreditationMetaW.cs
@@ -8,9 +8,11 @@ namespace RED.Models.ElectronicDiary
         public int Id { get; set; }
 
         [Required]
+        [Display(Name = "Регистриран")]
         public DateTime Registered { get; set; }
 
         [Required]
+        [Display(Name = "Валиден до")]
         public DateTime ValidTo { get; set; }
     }
 }
